033b9e6c5c9aa1f188f7365c7a7abd4ed2d2f415
Aditya T.L.N. <myselftlnaditya@gmail.com>
2025-08-18 14:10:42 +0530

    model training and hyper parameter tuning performed

--- source/components/data_ingestion.py
+++ source/components/data_ingestion.py
@@ -9,6 +9,9 @@ from dataclasses import dataclass
 from source.components.data_transformation import DataTransformation
 from source.components.data_transformation import DataTransformationConfig
 
+from source.components.model_trainer import ModelTrainerConfig
+from source.components.model_trainer import ModelTrainer
+
 @dataclass
 class DataIngestionConfig:
     train_data_path:str = os.path.join('artifacts',"train.csv")
@@ -50,5 +53,9 @@ class DataIngestion:
 if __name__ == "__main__":
     obj = DataIngestion()
     train_Data,test_Data = obj.initiate_data_ingestion()
+    
     data_transformation = DataTransformation()
-    data_transformation.initiate_data_transformation(train_Data,test_Data)
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_Data,test_Data)
+
+    model_trainer = ModelTrainer()
+    print(model_trainer.initiate_model_trainer(train_arr,test_arr))
